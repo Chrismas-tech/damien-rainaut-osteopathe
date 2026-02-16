c660d58c0bc2257ff7b2638b161ceee5abdb2456
Christmas-tech <lucianichristophe0@mail.com>
2026-02-16 20:40:50 +0100

    credits

--- resources/views/website/pages/credits.blade.php
+++ resources/views/website/pages/credits.blade.php
@@ -14,12 +14,6 @@
                     </a>
                 </div>
                 <div>
-                    <a
-                        href="https://fr.freepik.com/photos-gratuite/patient-osteopathie-recevant-massage-traitement_20148567.htm#fromView=search&page=1&position=15&uuid=13532c98-c9fe-4a58-9642-7849075614b5&query=osteopathe">Image
-                        n°2 - Freepik
-                    </a>
-                </div>
-                <div>
                     <a href="https://www.flaticon.com/fr/icones-gratuites/oreille" title="oreille icônes">Oreille icônes
                         créées par deemakdaksina - Flaticon
                     </a>
@@ -68,9 +62,6 @@
                     <a href="https://www.flaticon.com/fr/icones-gratuites/goutte-deau" title="goutte d'eau icônes">
                         Goutte
                         d'eau icônes créées par Adury5711 - Flaticon</a>
-                </div>
-                <div>
-                    <a href="http://www.freepik.com">Designed by vector_corp / Freepik</a>
                 </div>
             </div>
         </section>
