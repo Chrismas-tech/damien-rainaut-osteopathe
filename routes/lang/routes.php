844a404f6941b6a3f5465c8845586efa52ae48a6
Christmas-tech <lucianichristophe0@mail.com>
2026-02-23 12:20:23 +0100

    home page better

--- routes/lang/routes.php
+++ routes/lang/routes.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\LangController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/')->group(function () {
-    Route::get('/{locale}', [LangController::class, 'lang_switch'])->name('lang.switch');
-});
+Route::get('/{locale}', [LangController::class, 'lang_switch'])->name('lang.switch');
