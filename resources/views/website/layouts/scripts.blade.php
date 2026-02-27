9cfc2cc724e94a267bbeaee28f236a44ad34df11
Christmas-tech <lucianichristophe0@mail.com>
2026-02-27 16:44:30 +0100

    opti mobile

--- resources/views/website/layouts/scripts.blade.php
+++ resources/views/website/layouts/scripts.blade.php
@@ -1,4 +1,4 @@
 <!-- NanoGallery -->
-<script src="{{ asset('js/admin/jquery-3.2.1.min.js') }}"></script>
-<script src="{{ asset('js/admin/nanogallery2/jquery.nanogallery2.min.js') }}"></script>
+{{-- <script src="{{ asset('js/admin/jquery-3.2.1.min.js') }}"></script> --}}
+{{-- <script src="{{ asset('js/admin/nanogallery2/jquery.nanogallery2.min.js') }}"></script> --}}
 
