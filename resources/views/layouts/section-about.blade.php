1b51e5cd5a64578e37a58a272dbf4ad067a85c12
Christmas-tech <lucianichristophe0@mail.com>
2026-02-27 16:59:08 +0100

    opti mobile

--- resources/views/layouts/section-about.blade.php
+++ resources/views/layouts/section-about.blade.php
@@ -680,7 +680,7 @@
                         <div class="card shadow-lg border-0 overflow-hidden h-100 department-card">
                             <div class="row g-0 h-100">
                                 <div class="col-md-5">
-                                    <img src="{{ asset('img/website/bg/entreprise-2.webp') }}"
+                                    <img src="{{ asset('img/website/damien/entreprise-2.webp') }}"
                                         class="img-entreprise-2 img-fluid h-100 object-fit-cover"
                                         alt="Séance ostéopathie demi-journée prévention TMS en entreprise à Nice">
                                 </div>
